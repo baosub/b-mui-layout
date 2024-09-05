f8ec97a9760b84252e5d7760777c8b615ea4d865
Baldo <barrosoub@protonmail.com>
2024-09-04 22:09:48 -0600

    bground was modified

--- app/bground/bgroundcomponents/AdoubleImage.tsx
+++ app/bground/bgroundcomponents/AdoubleImage.tsx
@@ -72,12 +72,32 @@ const AdoubleImage = () => {
                     zIndex: 1, // Asegura que el contenido esté sobre la imagen de fondo
                 }}
             >
+
+
                 <Typography variant="h2" align="center" gutterBottom>
                     ¡Bienvenido!
                 </Typography>
                 <Typography variant="body1" align="center">
                     Esta es tu página de bienvenida con un fondo elegante y un diseño minimalista.
                 </Typography>
+
+
+
+                <Box sx={{ textAlign: 'center', marginTop: '50px', position: 'relative' }}>
+    <Typography
+      variant="h1"
+      sx={{
+        background: 'linear-gradient(90deg, #000000, #333333)', // Gradiente de negro a gris oscuro
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent', // Esto hace que el gradiente se aplique solo al texto
+        fontWeight: 'bold',
+      }}
+    >
+      Elegancia Minimalista
+    </Typography>
+  </Box>
+
+               
             </Box>
         </Box>
   )
